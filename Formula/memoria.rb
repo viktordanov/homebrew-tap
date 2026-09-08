1eaf730f73a5ce7c4690c2d0b17f8caa400b4f1f
Viktor Danov (Mac remote) <orangeleaf12@gmail.com>
2026-09-08 12:00:31 +0200

    fix: select release archive by host architecture

--- Formula/memoria.rb
+++ Formula/memoria.rb
@@ -1,18 +1,14 @@
 class Memoria < Formula
   desc "Keeps project documentation connected to the code it explains"
   homepage "https://github.com/viktordanov/homebrew-tap"
+  archive_arch = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+  url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.1.0/" \
+      "memoria-v0.1.0-#{archive_arch}-apple-darwin.tar.gz"
   version "0.1.0"
+  sha256 Hardware::CPU.arm? ?
+    "36bd0935cb75b121a314c75e18c585e0344766e27a03d2a06604aaf30171ff6e" :
+    "f0f6dfb471f0863e0f7d4a7504e1f58e9e9a333a31a5f8a67d45ad1c5f1bbacd"
   license "MIT"
-
-  on_arm do
-    url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.1.0/memoria-v0.1.0-aarch64-apple-darwin.tar.gz"
-    sha256 "36bd0935cb75b121a314c75e18c585e0344766e27a03d2a06604aaf30171ff6e"
-  end
-
-  on_intel do
-    url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.1.0/memoria-v0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "f0f6dfb471f0863e0f7d4a7504e1f58e9e9a333a31a5f8a67d45ad1c5f1bbacd"
-  end
 
   depends_on macos: :ventura
 
