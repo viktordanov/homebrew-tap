1eaf4b10c9f08f0f589a333855ddcd1600e35417
Viktor Danov (Mac remote) <orangeleaf12@gmail.com>
2026-09-08 12:03:09 +0200

    style: simplify architecture checksum selection

--- Formula/memoria.rb
+++ Formula/memoria.rb
@@ -5,9 +5,9 @@ class Memoria < Formula
   url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.1.0/" \
       "memoria-v0.1.0-#{archive_arch}-apple-darwin.tar.gz"
   version "0.1.0"
-  sha256 Hardware::CPU.arm? ?
-    "36bd0935cb75b121a314c75e18c585e0344766e27a03d2a06604aaf30171ff6e" :
-    "f0f6dfb471f0863e0f7d4a7504e1f58e9e9a333a31a5f8a67d45ad1c5f1bbacd"
+  arm64_sha256 = "36bd0935cb75b121a314c75e18c585e0344766e27a03d2a06604aaf30171ff6e"
+  x86_64_sha256 = "f0f6dfb471f0863e0f7d4a7504e1f58e9e9a333a31a5f8a67d45ad1c5f1bbacd"
+  sha256 Hardware::CPU.arm? ? arm64_sha256 : x86_64_sha256
   license "MIT"
 
   depends_on macos: :ventura
