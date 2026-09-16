1eaf1b42b98c492ec35137fc71410538014404a5
Viktor Danov (Mac remote) <orangeleaf12@gmail.com>
2026-09-16 12:02:40 +0300

    memoria 0.6.0

--- Formula/memoria.rb
+++ Formula/memoria.rb
@@ -2,10 +2,10 @@ class Memoria < Formula
   desc "Keeps project documentation connected to the code it explains"
   homepage "https://github.com/viktordanov/homebrew-tap"
   archive_arch = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-  url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.5.0/" \
-      "memoria-v0.5.0-#{archive_arch}-apple-darwin.tar.gz"
-  arm64_sha256 = "7ef20c9bb92bfffa606ffd3535e43a72bdeaae2b7502fb0d210ac8c8af1c2060"
-  x86_64_sha256 = "dc362610708eb272f15395b99814e9be3b00381b9d96a437f1e0075dd15fd73e"
+  url "https://github.com/viktordanov/homebrew-tap/releases/download/memoria-v0.6.0/" \
+      "memoria-v0.6.0-#{archive_arch}-apple-darwin.tar.gz"
+  arm64_sha256 = "a2cb6fd77a9cdd13a576882444e933fe01afdff0e42c0902aa32c63d89c83e5e"
+  x86_64_sha256 = "6630d4dfc7499844dfd62f6aa3a2147d9793edfa4ae883d10272fc07a933d57d"
   sha256 Hardware::CPU.arm? ? arm64_sha256 : x86_64_sha256
   license "MIT"
 
@@ -16,6 +16,6 @@ class Memoria < Formula
   end
 
   test do
-    assert_match "memoria 0.5.0", shell_output("#{bin}/memoria --version")
+    assert_match "memoria 0.6.0", shell_output("#{bin}/memoria --version")
   end
 end
